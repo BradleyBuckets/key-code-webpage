e30ded7987526b7ea4bc12f07ac69a7a59528d8a
Bradley Griffin <bigriff11@gmail.com>
2022-11-10 06:51:01 -0700

    Add way to get spacebar name

--- keycode.js
+++ keycode.js
@@ -2,6 +2,8 @@ let text = document.querySelector('.text')
 let elementName = document.querySelector('.name')
 
 window.addEventListener("keydown", e => {
-    text.textContent = e.keyCode
-    elementName.textContent = `Letter ${e.key.toUpperCase()}`
+    text.textContent = e.keyCode;
+    let keyName = e.key.toUpperCase();
+    if (e.keyCode === 32) keyName = 'SPACEBAR';
+    elementName.textContent = `Key ${keyName}`;
 } )
