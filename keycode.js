6b6101fab281bbe26a7096dcaf566f2eeda6cc7c
Bradley Griffin <bigriff11@gmail.com>
2022-11-10 06:57:23 -0700

    Imporve logic for items in the code

--- keycode.js
+++ keycode.js
@@ -1,7 +1,7 @@
 let text = document.querySelector('.text')
 let elementName = document.querySelector('.name')
 
-window.addEventListener("keydown", e => {
+document.addEventListener("keyup", e => {
     text.textContent = e.keyCode;
     let keyName = e.key.toUpperCase();
     if (e.keyCode === 32) keyName = 'SPACEBAR';
